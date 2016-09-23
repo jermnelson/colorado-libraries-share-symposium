091bfc56e0aa97f01181e3fbe6da6fa5f7953eb7
Jeremy Nelson <jermnelson@gmail.com>
2016-09-22 23:02:15 -0600

    moved app module to __init__

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
-from app import app
+__author__ = "Jeremy Nelson"
+
+import rdflib
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9912)
